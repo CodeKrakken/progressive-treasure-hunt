ec80e6edb0afd65fab4975b5b7aacf953b7f6fef
CodeKrakken <newholm1@hotmail.com>
2021-02-07 14:36:31 +0000

    Add empty calculate function

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -1,3 +1,13 @@
 'use strict'
 
 const Calculator = require('../lib/Calculator')
+let calculator
+describe('calculator', function() {
+  beforeEach(function() {
+    calculator = new Calculator
+  })
+
+  it('responds to calculate function', function() {
+    expect(calculator.calculate).toBeDefined()
+  })
+})
