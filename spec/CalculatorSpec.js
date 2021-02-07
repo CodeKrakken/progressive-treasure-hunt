6239291a4203d376914842ada4bf148c0bbabfae
CodeKrakken <newholm1@hotmail.com>
2021-02-07 17:44:40 +0000

    Sort and reverse an array of gems

--- spec/CalculatorSpec.js
+++ spec/CalculatorSpec.js
@@ -27,4 +27,16 @@ describe('calculator', function() {
   it('can identify an oversized gem that prevents fair division', function() {
     expect(calculator.calculate([12,3,4], 3)).toEqual('Cannot be split equally.')
   })
+
+  it('can sort and reverse an array of gems', function() {
+    calculator.calculate([3, 6, 4, 12, 34, 9], 2)
+    expect(calculator.gems).toEqual([34, 12, 9, 6, 4, 3])
+  })
+
+  // it('can fulfil examples from spec', function() {
+  //   expect(calculator.calculate([4,4,4], 3)).toEqual('Split successful.')
+  //   expect(calculator.calculate([27,7,20], 2)).toEqual('Split successful.')
+  //   expect(calculator.calculate([6,3,2,4,1], 2)).toEqual('Split successful.')
+  //   expect(calculator.calculate([3,2,7,7,14,5,3,4,9,2], 4)).toEqual('Split successful.')
+  // })
 })
